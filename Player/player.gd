506ac788b046c38d1797c20b239c9c74f4f6ce17
WIN-9V64E6COKSA\user <pockerhead@ya.ru>
2025-04-28 12:54:56 +0300

    initial

---
+++ Player/player.gd
@@ -0,0 +1,101 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Stores the x/y direction the player is trying to look in
+var _look := Vector2.ZERO
+# Stores the direction the player moves when attacking
+var _attack_direction = Vector3.ZERO
+
+@export var mouse_sensitivity: float = 0.00075
+@export var min_boundary: float = -60
+@export var max_boundary: float = 10
+@export var animation_decay: float = 20.0
+@export var attack_move_speed: float = 3.0
+
+@onready var horizontal_pivot: Node3D = $HorizontalPivot
+@onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+@onready var rig_pivot: Node3D = $RigPivot
+@onready var rig: Node3D = $RigPivot/Rig
+
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	frame_camera_rotation()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var local_speed = SPEED
+	
+	if Input.is_action_pressed("sprint"):
+		local_speed *= 1.5
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := get_movement_direction()
+	rig.update_animation_tree(direction)
+	if direction:
+		velocity.x = direction.x * local_speed
+		velocity.z = direction.z * local_speed
+		look_toward_direction(direction, delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, local_speed)
+		velocity.z = move_toward(velocity.z, 0, local_speed)
+	handle_slashing_physics_frame(delta)
+	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED && event is InputEventMouseMotion:
+		_look = -event.relative * mouse_sensitivity
+		print(_look)
+	
+	if rig.is_idle() && Input.is_action_pressed("click"):
+			slash_attack()
+
+func get_movement_direction() -> Vector3:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_vector :=  Vector3(input_dir.x, 0, input_dir.y).normalized()
+	var direction := horizontal_pivot.global_transform.basis * input_vector
+	return direction
+
+func frame_camera_rotation() -> void:
+	horizontal_pivot.rotate_y(_look.x)
+	vertical_pivot.rotate_x(_look.y)
+	
+	vertical_pivot.rotation.x = clampf(vertical_pivot.rotation.x, deg_to_rad(min_boundary), deg_to_rad(max_boundary))
+	$SpringArm3D.global_transform = vertical_pivot.global_transform
+	_look = Vector2.ZERO
+
+func look_toward_direction(direction: Vector3, delta: float) -> void: 
+	var target_transform := rig_pivot.global_transform.looking_at(
+		rig_pivot.global_position + direction, Vector3.UP, true
+	)
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(
+		target_transform,
+		1.0 - exp(-animation_decay * delta)
+	)
+	
+func slash_attack() -> void:
+	rig.travel("Slash")
+	_attack_direction = get_movement_direction()
+	if _attack_direction.is_zero_approx():
+		_attack_direction = rig.global_basis * Vector3.BACK
+	
+func handle_slashing_physics_frame(delta: float):
+	if not rig.is_slashing():
+		return
+	velocity.x = _attack_direction.x * attack_move_speed
+	velocity.z = _attack_direction.z * attack_move_speed
+	look_toward_direction(_attack_direction, delta)
---
+++ Player/rig.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@export var animation_speed: float = 10.0
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree[playback_path]
+
+var run_path: String = "parameters/MoveSpace/blend_position"
+var playback_path: String = "parameters/playback"
+var run_weight_target := -1.0
+
+
+func _physics_process(delta: float) -> void:
+	animation_tree[run_path] = move_toward(
+		animation_tree[run_path],
+		 run_weight_target,
+		 delta * animation_speed
+	)
+
+func update_animation_tree(direction: Vector3) -> void:
+	if direction.is_zero_approx():
+		run_weight_target = -1
+	else:
+		run_weight_target = 1
+
+func travel(animation_name: String) -> void:
+	playback.travel(animation_name)
+	
+func is_idle() -> bool:
+	return playback.get_current_node() == "MoveSpace"
+
+func is_slashing() -> bool:
+	return playback.get_current_node() == "Slash"
